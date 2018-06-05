0f57e142427d845edad85a79a1754d9a1ddc2dbf
Aimee Barciauskas <aimee@developmentseed.org>
2018-06-05 19:08:31 -0400

    Update code to upload stats files to S3 and delete extracted files

--- lambda_function.py
+++ lambda_function.py
@@ -1,12 +1,28 @@
 import parallel_wget
 import tif_stats
+import boto3
+import os
+
+client = boto3.client('s3')
 
 def lambda_handler(event, context):
     config = event['config']
-    parallel_wget.parallel_wget(
+    # Return a list of files
+    extracted_files = parallel_wget.parallel_wget(
        host=config['provider']['host'],
        path=config['collection']['provider_path'],
        files=event['input']
     )
-    tif_stats.generate_stats()
+    # Return csv file names
+    stats_files = tif_stats.generate_stats()
+    # Upload to S3
+    dest_bucket = config['buckets']['protected']
+    file_prefix = 'sezu-stats/'
+    map(lambda f: client.put_object(
+      Bucket = dest_bucket,
+      Key = '{0}{1}'.format(file_prefix, f),
+      Body = open(f, 'r').read())
+    , stats_files)
+    # Delete all files
+    map(os.remove, extracted_files)
     return {'messsage': 'processing complete'}
